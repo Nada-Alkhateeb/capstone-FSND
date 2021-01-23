c717aca3237101dfd089d264873bcda802710374
Nada-Alkhateeb <nada.khateeb9@gmail.com>
2021-01-24 01:58:53 +0300

    for commit

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ export ALGORITHMS = ['RS256']
 export API_AUDIENCE = 'capstone'
 export CLIENT_ID = 'NDBBw6BIfDQQkhpKvWx294tcPNXudn72'
 export CALL_BACK_URL= 'http://flask-movies-deploy.herokuapp.com/'
-export DATABASE_URL='postgres://postgres:1415@localhost:5432/movies'
+export DATABASE_URL='postgres://utjbteeopxbijn:b890db95043b2049113c7e32345e5b53d2fa775b13b8ba97a51e6f48759bf7e4@ec2-54-85-13-135.compute-1.amazonaws.com:5432/d4ih77eqh0c9f5'
