b4d7a753f8cda2ff8a0eec531abc3d0cffb8a970
Nada-Alkhateeb <nada.khateeb9@gmail.com>
2021-01-24 00:55:57 +0300

    thr commit

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ export AUTH0_DOMAIN = 'fsnd-dom.us.auth0.com'
 export ALGORITHMS = ['RS256']
 export API_AUDIENCE = 'capstone'
 export CLIENT_ID = 'NDBBw6BIfDQQkhpKvWx294tcPNXudn72'
-export CALL_BACK_URL= 'http://127.0.0.1:5000/'
+export CALL_BACK_URL= 'http://flask-movies-deploy.herokuapp.com/'
 export DATABASE_URL='postgres://postgres:1415@localhost:5432/movies'
